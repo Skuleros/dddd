c1bfc73db51ae2452c5989457468874b69d44618
Student WSB <edziuo11pl@gmail.com>
2020-03-29 11:23:34 +0200

    make it better

---
+++ js/func.js
@@ -0,0 +1,16 @@
+// alert("Hello")
+
+//const example = () =>{
+  //  const fromInput = document.getElementById("example").value;
+    //alert(fromInput);
+    //document.getElementById("p")[0].innerText = fromInput;
+//}
+
+//document.getElementByid("action").addEventListener("click", example)
+
+const someTitle = "new example";
+
+module.exports={someTitle}
+
+
+
